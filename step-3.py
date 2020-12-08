e29f7c57cc457375fa7a5d64b32ec65a9987c379
Ray <ddarjae@naver.com>
2020-12-09 08:42:00 +0900

    Complete printing 6x3x3 matrix cube finally complete printing Rubik's cube by seperating for-loop

--- step-3.py
+++ step-3.py
@@ -6,21 +6,48 @@ def matrix_6x3x3(cube):
             for _ in range(3):
                 cube[i][j].append(color_str[i])
 
-    for i in range(6):
-        print()
+    for i in range(1):
         for j in range(3):
-            if i==0 or i==5:
-                print(' '*15, end='')
-            elif i==2:
-                print(' '*10, end='')
-            elif i==3:
-                print(' '*21, end='')
-            elif i==4:
-                print(' '*32, end='')
+            print(' '*15, end='')
+            for k in range(3):
+                    print(cube[i][j][k], end=' ')
+            print()
+    print()
+    
+    for i in range(1, 5):
+        for j in range(1):
+            if i != 1:
+                print(' '*4, end='')
             for k in range(3):
                 print(cube[i][j][k], end=' ')
-            print()
+    if cube[i][j][k] == cube[4][0][2]:
+        print()
             
-    
+    for i in range(1, 5):
+        for j in range(1, 2):
+            if i != 1:
+                print(' '*4, end='')
+            for k in range(3):
+                print(cube[i][j][k], end=' ')
+    if cube[i][j][k] == cube[4][0][2]:
+        print()
+
+    for i in range(1, 5):
+        for j in range(2, 3):
+            if i != 1:
+                print(' '*4, end='')
+            for k in range(3):
+                print(cube[i][j][k], end=' ')
+    if cube[i][j][k] == cube[4][0][2]:
+        print()
+
+    for i in range(5, 6):
+        print()
+        for j in range(3):
+            print(' '*15, end='')
+            for k in range(3):
+                    print(cube[i][j][k], end=' ')
+            print()
+        
 cube = [[[] for _ in range(3)]for _ in range(6)]
 matrix_6x3x3(cube)
