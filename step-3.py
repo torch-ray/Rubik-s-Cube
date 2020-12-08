96cb7bd2f5ae67c2c78a51bc08b1e76ff0d76cd1
Ray <ddarjae@naver.com>
2020-12-08 19:25:56 +0900

    Change the way to print cube matrix made each function for the color to print cube matrix but, it would not be useful to bring the step-2

--- step-3.py
+++ step-3.py
@@ -1,8 +1,25 @@
-cube = [[[] for _ in range(3)]for _ in range(6)]
-color_str="WBGYOR"
-
-for i in range(6):
-    for j in range(3):
+def matrix_B(cube):
+    for i in range(3):
         for _ in range(3):
-            cube[i][j].append(color_str[i])
-print(cube)
+            cube[i].append("B")
+
+    for i in range(3):
+        print(' '*9, end='')
+        for j in range(3):
+            print(cube[i][j], end=' ')
+        print()
+cube1 = [[] for _ in range(3)]
+matrix_B(cube1)
+
+def matrix_W(cube):
+    for i in range(3):
+        for _ in range(3):
+            cube[i].append("W")
+
+    for i in range(3):
+        for j in range(3):
+            print(cube[i][j], end=' ')
+        print()
+cube2 = [[] for _ in range(3)]
+matrix_W(cube2)
+
